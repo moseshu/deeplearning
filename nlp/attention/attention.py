e1fb76e7fabfaaee69428cb58783a7d9867e2853
hujunjie2 <hujunjie2@tal.com>
2022-05-26 20:50:30 +0800

    添加decoder和attention

---
+++ nlp/attention/attention.py
@@ -0,0 +1,72 @@
+import math
+import torch
+import torch.nn as nn
+from typing import Optional, Tuple
+
+Past = Tuple[torch.Tensor, torch.Tensor]
+
+
+
+class LauongAttention(nn.Module):
+    def __init__(self, hidden_enc_size, hidden_dec_size, method="concat"):
+        super(LauongAttention, self).__init__()
+        self.hidden_dec_size = hidden_dec_size
+        self.hidden_enc_size = hidden_enc_size
+        self.method = method
+        self.V = nn.Parameter(torch.rand(hidden_dec_size))
+        self.W = nn.Parameter(torch.rand(hidden_enc_size))
+        if method == "concat":
+            self.fc = nn.Linear(hidden_enc_size + hidden_dec_size, hidden_dec_size, bias=False)
+        elif method == "dot":
+            assert hidden_dec_size == hidden_enc_size
+            self.fc = nn.Linear(hidden_enc_size, hidden_dec_size, bias=False)
+        elif method == "general":
+            self.fc = nn.Linear(hidden_dec_size, hidden_enc_size, bias=False)
+        elif method == "sum":
+            assert hidden_dec_size == hidden_enc_size
+            self.fc = nn.Linear(hidden_enc_size, hidden_dec_size, bias=False)
+        else:
+            raise ValueError("check the method value ,method value is one of  concat,dot,general,sum ")
+
+    def forward(self, decoder_hidden: Tensor, encoder_out: Tensor):
+        """
+
+        :param: decoder_hidden: [n_layers, batch, hidden_dec_size]
+        :param: encoder_out: [batch, seq, hidden_enc_size]
+        :return: att_weights: [batch, seq_len]
+        """
+        # Calculating Alignment Scores Note: encoder hidden can be used as decoder hidden
+        decoder_hidden = decoder_hidden[-1]  # [batch, hidden_dec_size] the last layer hidden
+        print(decoder_hidden.shape)
+        batch_size = encoder_out.size(0)
+        seq_len = encoder_out.size(1)
+        if self.method == "concat":
+            decoder_hidden = decoder_hidden.unsqueeze(1).repeat(1, seq_len, 1)  # [batch, seq_len, hidden_dec_size]
+            cat_hidden = torch.cat([decoder_hidden, encoder_out], dim=2)
+            print(cat_hidden.shape)
+            tanh_ws = torch.tanh(self.fc(cat_hidden))  # [batch_size, hidden_dec_size, seq_len]
+            tanh_ws = tanh_ws.permute(0, 2, 1)
+            V = self.V.repeat(batch_size, 1).unsqueeze(1)  # [batch_size, 1, hidden_dec_size]
+            E = V.bmm(tanh_ws).squeeze(1)  # [batch_size, seq_len]
+            att_weights = torch.softmax(E, dim=1)  # [batch_size, seq_len]
+            # scores = att_weights.unsqueeze(1).bmm(encoder_out) #[batch_size, 1, hidden_enc_size]
+
+        elif self.method == "dot":
+            dec_hidden = decoder_hidden.unsqueeze(1)
+            att_weights = dec_hidden.bmm(encoder_out.permute(0, 2, 1))
+            att_weights = att_weights.squeeze(1)
+            att_weights = torch.softmax(att_weights, dim=1)
+
+        elif self.method == "general":
+            dec_hidden = self.fc(decoder_hidden).unsqueeze(1)  # [batch,1, hidden_enc_size]
+            att_weights = dec_hidden.bmm(encoder_out.permute(0, 2, 1))  # [batch, 1, seq_len]
+            att_weights = att_weights.squeeze(1)
+            att_weights = torch.softmax(att_weights, dim=1)
+        elif self.method == "sum":
+            decoder_hidden = decoder_hidden.unsqueeze(1).repeat(1, seq_len, 1)
+            out = self.fc(decoder_hidden + encoder_out)
+            W = self.W.unsqueeze(0).repeat(batch_size, 1).unsqueeze(1)
+            att_weights = W.bmm(out.permute(0, 2, 1))
+            att_weights = att_weights.squeeze(1)
+            att_weights = torch.softmax(att_weights, dim=1)
+        return att_weights  # [batch, seq_len]
---
+++ nlp/models/decoder.py
@@ -0,0 +1,73 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from encoder import EncoderRNNS
+from typing import Tuple
+from ..attention.attention import LauongAttention
+class BahdanauDecoder(nn.Module):
+    supported_rnns = {
+        'lstm': nn.LSTM,
+        'gru': nn.GRU,
+        'rnn': nn.RNN,
+    }
+
+    def __init__(self, vocab_size, emb_dim, hidden_enc_size, hidden_dec_size,
+                 n_layers=1,
+                 rnn_type='lstm',
+                 drop_prob=0,
+                 bidirectional=False):
+        super(BahdanauDecoder, self).__init__()
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, emb_dim)
+        self.attention = LauongAttention(hidden_enc_size, hidden_dec_size)
+        self.rnn_type = rnn_type.lower()
+        rnn_cell = self.supported_rnns[rnn_type.lower()]
+        self.rnn = rnn_cell(
+            input_size=emb_dim + hidden_enc_size,
+            hidden_size=hidden_dec_size,
+            num_layers=n_layers,
+            bias=True,
+            batch_first=True,
+            dropout=drop_prob,
+            bidirectional=bidirectional,
+        )
+        self.classfier = nn.Linear(emb_dim + hidden_enc_size + hidden_dec_size, vocab_size)
+
+        self.dropout = nn.Dropout(drop_prob)
+
+    def forward(self, target: Tensor, hidden_dec, encoder_out: Tensor):
+        """
+
+        :param target: [batch]
+        :param hidden_dec: rnn,gru shape is [n_layers, batch, hidden_dec_size],lstm shape is (hn, cn)
+        :param encoder_out: [batch, seq_len, hidden_enc_size]
+        :return:
+        """
+        ######################## 1. TARGET EMBEDDINGS #########################
+        target = target.unsqueeze(1)  # [b, 1] : single word
+        print(target)
+        embedded_trg = self.embedding(target)  # [batch, 1, emb_size]
+
+        ################## 2. CALCULATE ATTENTION WEIGHTS #####################
+        if self.rnn_type == "lstm":
+            # [batch, seq_len]
+            att_weights = self.attention(hidden_dec[0], encoder_out)  # hidden_enc shape is (hn, cn)
+        else:
+            att_weights = self.attention(hidden_dec, encoder_out)
+        att_weights = att_weights.unsqueeze(1)  # [batch, 1, eq_len]
+
+        ###################### 3. CALCULATE WEIGHTED SUM ######################
+        context_vector = att_weights.bmm(encoder_out)  # [batch, 1, hidden_enc_size]
+
+        ############################# 4. RNN or GRU or LSTM LAYER ############################
+        rnn_input = torch.cat([embedded_trg, context_vector], dim=2)
+        dec_out, hiden_dec = self.rnn(rnn_input, hidden_dec)
+        # dec_out shape is [batch, 1, hidden_dec_size]
+        final_input = torch.cat([embedded_trg.squeeze(1),
+                                 context_vector.squeeze(1),
+                                 dec_out.squeeze(1)
+                                 ], dim=1)
+        output = self.classfier(final_input)  # [batch, target_vocab_size]
+        return output, hiden_dec, att_weights
+
