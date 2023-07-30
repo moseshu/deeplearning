ebdb7f0511ad7dc682dbd36ffa2bda3f8fc9a7c0
Moses Hu <moses25@126.com>
2023-07-30 21:58:19 +0800

    Update qlora_llama2_sft.sh

--- llama-finetune/qlora_llama2_sft.sh
+++ llama-finetune/qlora_llama2_sft.sh
@@ -3,7 +3,7 @@ peft_path='lora-checkpoint/adapter_model'
 #torchrun --nproc_per_node=2 --master_port=20001 
 #--deepspeed config/ds_config.json \
 #deepspeed --num_gpus=2 
-torchrun --nproc_per_node=1 --master_port=20002 llama2_sft_qlora.py \
+torchrun --nproc_per_node=8 --master_port=20002 llama2_sft_qlora.py \
     --base_model ${base_model} \
     --batch_size 16 \
     --data_path ../data/shopping \
@@ -13,7 +13,7 @@ torchrun --nproc_per_node=1 --master_port=20002 llama2_sft_qlora.py \
     --group_by_length \
     --val_set_size 0 \
     --lora_r 64 \
-    --cutoff_len 2048 \
+    --cutoff_len 4096 \
     --learning_rate 1e-4 \
     --lora_alpha 128 \
     --lora_target_modules '[q_proj,k_proj,v_proj,o_proj,gate_proj,down_proj,up_proj]' \
