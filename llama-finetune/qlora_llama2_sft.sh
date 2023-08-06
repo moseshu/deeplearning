9dfa8c948979cc6b91f69541bf7a1e959c609ae7
Moses Hu <moses25@126.com>
2023-08-06 23:27:17 +0800

    Update qlora_llama2_sft.sh

--- llama-finetune/qlora_llama2_sft.sh
+++ llama-finetune/qlora_llama2_sft.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-base_model='../llama_weight/Llama-2-7b-chat-hf'
-peft_path='checkpoint_qlora_shop/checkpoint-9000/adapter_model' 
-torchrun --nproc_per_node=4 --master_port=20001 qlora_sft_chat.py \
---base_model ${base_model} \
---data_path "../data/conversation-data" \
---lora_r 64 \
---output_dir checkpoint_chat \
---fp16 True \
---num_epochs 4 \
---lora_target_modules "q_proj,k_proj,v_proj,o_proj,gate_proj,down_proj,up_proj" \
---use_4bit True \
---format_prompt "llama2" \
---max_seq_length 2048
+base_model=meta-llama/Llama-2-7b-hf
+peft_path='checkpoint_chat/checkpoint-1600/adapter_model' 
+#--deepspeed config/ds_config.json \
+#deepspeed --num_gpus=2 
+CUDA_VISIBLE_DEVICES=2 torchrun --nproc_per_node=1 --master_port=20005  \
+    --base_model ${base_model} \
+    --batch_size 16 \
+    --data_path ../data/zhijian \
+    --micro_batch_size 10 \
+    --num_epochs 10 \
+    --output_dir checkpoint_qlora \
+    --group_by_length \
+    --val_set_size 0 \
+    --lora_r 64 \
+    --cutoff_len 4096 \
+    --learning_rate 3e-5 \
+    --lora_alpha 128 \
+    --lora_target_modules '[q_proj,k_proj,v_proj,o_proj,gate_proj,down_proj,up_proj]' \
+    --report_to 'tensorboard' \
+    --format_prompt 'llama2'
 
